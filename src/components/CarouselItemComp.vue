582c68d4be81e36d3cf1d81918e9397cd4cd5561
Насонов Александр Геннадьевич <anasonov@nevalab.ru>
2023-07-12 15:54:28 +0300

    end

---
+++ src/components/CarouselItemComp.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="carousel_item">
+<img :src=" require('../assets/bedroom/'+item_data.img)" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CarouselItemComp',
+
+    props: {
+        item_data: {
+            type: Object,
+            default: () => { }
+        }
+    },
+    data() {
+        return {
+
+        };
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
